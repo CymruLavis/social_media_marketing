743943855c7cd1ff39735dc965232fa50286a52a
Ethan <elavis00@outlook.com>
2025-02-15 18:11:49 -0800

    successfully subscribed to comment webhook

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-from fastapi import FastAPI
 import logging
 logging.basicConfig(
     level=logging.INFO,  # Set the log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
@@ -7,6 +6,12 @@ logging.basicConfig(
         logging.StreamHandler()  # Output logs to the terminal
     ],
 )
+
+from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse, HTMLResponse
+import json
+import os
+
 # logger = logging.getLogger("my_fastapi_app")
 
 app = FastAPI()
@@ -16,7 +21,44 @@ def root():
     logging.info("We made it to the root")
     return {"status": "Success"}
 
-@app.get('/comment')
-def root():
-    logging.info("We made it to the comment")
-    return {"status": "Success"}
+@app.route('/privacy_policy')
+def privacy_policty(request: Request):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(base_dir, "privacy_policy.html")
+    with open(file_path, "rb") as f:
+        privacy_policty_html = f.read()
+    return HTMLResponse(content=privacy_policty_html)
+
+@app.route('/comment', methods=['GET', 'POST'])
+async def root(request: Request):
+    if request.method == "POST":
+        try:
+            request_data = await request.json()
+            logging.info(f"Recieved: {request_data.get("entry")}")
+
+            return PlainTextResponse(content="Success", status_code=200)
+            
+            # requrest_data = request.query_params.get("field")
+            # logging.info(f"THE CAPTURED DATA IS: {requrest_data}")
+            # return PlainTextResponse(content="POST recieved", status_code=200)
+        except Exception as e:
+            logging.error(f"Error processing POST request: {e}")
+            return PlainTextResponse(content="FAILURE", status_code=400)
+
+    if request.method == "GET":
+        hub_mode = request.query_params.get('hub.mode')
+        hub_challenge = request.query_params.get('hub.challenge')
+        hub_verify_token = request.query_params.get('hub.verify_token')
+
+        logging.info("We made it to the comment")
+
+        logging.info(f"Hub Mode: {hub_mode}")
+        logging.info(f"Hub Challenge: {hub_challenge}")
+        logging.info(f"Hub Verify Token: {hub_verify_token}")
+
+        if hub_challenge:
+            logging.info("Verfied")
+            logging.info(f"{hub_challenge} is of type {type(hub_challenge)}")
+            return PlainTextResponse(content=hub_challenge)  # Echo the hub.challenge value
+        else:
+            return "<p>Verification Failed</p>"
--- tests/test_local.py
+++ tests/test_local.py
@@ -1,8 +1,17 @@
+import logging
+logging.basicConfig(
+    level=logging.INFO,  # Set the log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler()  # Output logs to the terminal
+    ],
+)
+
+from urllib.parse import urlencode
 import httpx
 import pytest
-import logging
 import json
-
+from fastapi import Request
 # This termincal command is to run a local server
 # poetry run python -m uvicorn src.main:app --reload --log-level info
 """
@@ -11,6 +20,7 @@ Use this file for when you're making calls against a running container built wit
 BASE_URL = "http://127.0.0.1:8000/{endpoint}" # update this if your url is different, you should see it when runing `just run_local`
 INDEX = BASE_URL.format(endpoint="")
 WEBHOOK_ENDPOINT = BASE_URL.format(endpoint="comment")
+PRIVACY_POLICY_ENDPOINT = BASE_URL.format(endpoint="privacy_policy")
 
 
 def test_uvicorn_container_call():
@@ -23,11 +33,47 @@ def test_uvicorn_container_call():
     assert response.status_code == 200
     assert response.json() == {"status": "Success"} # This is the basic response code set up already
 
-def test_webhood():
+def test_webhook_get():
+    query_params = {
+        "hub.mode": "subscribe",
+        "hub.challenge": "557837336",
+        "hub.verify_token": "1234",
+    }
+    # Encode the query parameters into a URL
+    webhook_url = f"{WEBHOOK_ENDPOINT}?{urlencode(query_params)}"
     with httpx.Client(verify=False) as client:
         response = client.get(
-            WEBHOOK_ENDPOINT,
+            webhook_url,
             timeout=30
         )
     logging.info(f"Response Content: {response.content}")
     assert response.status_code == 200
+
+def test_webhook_post():
+    query_params = {
+        "hub.mode": "subscribe",
+        "hub.challenge": "557837336",
+        "hub.verify_token": "1234",
+    }
+    # Encode the query parameters into a URL
+    webhook_url = f"{WEBHOOK_ENDPOINT}?{urlencode(query_params)}"
+    with httpx.Client(verify=False) as client:
+        response = client.post(
+            webhook_url,
+            timeout=30
+        )
+    logging.info(f"Response Content: {response.content}")
+    assert response.status_code == 200
+
+def test_privacy_policy():
+    with httpx.Client(verify=False) as client:
+        response = client.get(
+            PRIVACY_POLICY_ENDPOINT,
+            timeout=30
+        )
+    logging.info("I hath returned")
+    logging.info(f"Response Content: {response.text}")
+    assert response.status_code == 200
+
+def test_send_dm():
+    pass
